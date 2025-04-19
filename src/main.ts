81cba0d4ebf8c98d64bc778af35f69fc9a61eb19
archilouk <a.loukas@thelan.gr>
2025-04-19 16:24:52 +0300

    Tailwind installation and setup

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App/App.vue'
-import router from './router.ts'
+import App from './App/App.vue';
+import router from './router.ts';
+import './@plugins/tailwind.css';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
